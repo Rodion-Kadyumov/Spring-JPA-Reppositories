0db481cd672d2799f83d3ad93f58816f1f66902c
Rodion Kadyumov <krrkes@gmail.com>
2023-04-28 12:11:59 +0500

    На проверку

---
+++ dao-hibernate-spring-boot-jpa_repository/src/main/resources/import.sql
@@ -0,0 +1,32 @@
+INSERT INTO netology.person (first_name, last_name, age, phone_number, city_of_living)
+values ('Rodion', 'Kaduymov', 36, +79000248128, 'Ekaterinburg');
+
+INSERT INTO netology.person (first_name, last_name, age, phone_number, city_of_living)
+values ('Alina', 'Fausova', 21, +79824561895, 'Ekaterinburg');
+
+INSERT INTO netology.person (first_name, last_name, age, phone_number, city_of_living)
+values ('Natalya', 'Fetisova', 35, +79000547625, 'Magnitogorsk');
+
+INSERT INTO netology.person (first_name, last_name, age, phone_number, city_of_living)
+values ('Alia', 'Naimbanbaeva', 38, +794554278654, 'Nadym');
+
+INSERT INTO netology.person (first_name, last_name, age, phone_number, city_of_living)
+values ('Anisa', 'Musina', 36, +79078954367, 'Orenburg');
+
+INSERT INTO netology.person (first_name, last_name, age, phone_number, city_of_living)
+values ('Fedor', 'Shtirkin', 34, +79826121212, 'Magnitogorsk');
+
+INSERT INTO netology.person (first_name, last_name, age, phone_number, city_of_living)
+values ('Vasya', 'Tokarev', 35, +79824561895, 'Ekaterinburg');
+
+INSERT INTO netology.person (first_name, last_name, age, phone_number, city_of_living)
+values ('Anna', 'Stepanova', 35, +79190620487, 'Ufa');
+
+INSERT INTO netology.person (first_name, last_name, age, phone_number, city_of_living)
+values ('Galymjan', 'Yakshibaev', 38, +79191157402, 'Magnitogorsk');
+
+INSERT INTO netology.person (first_name, last_name, age, phone_number, city_of_living)
+values ('Ramyl', 'Valetov', 34, +79078954367, 'Orenburg');
+
+INSERT INTO netology.person (first_name, last_name, age, phone_number, city_of_living)
+values ('Dmitrii', 'Ekimov', 36, +79292696206, 'Tymen');
---
+++ dao-hibernate-spring-boot-jpa_repository/src/test/java/ru/netology/daohibernatespringboot/DaoHibernateSpringBootApplicationTests.java
@@ -0,0 +1,11 @@
+package ru.netology.daohibernatespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaoHibernateSpringBootApplicationTests {
+  @Test
+  void contextLoads() {
+  }
+}
